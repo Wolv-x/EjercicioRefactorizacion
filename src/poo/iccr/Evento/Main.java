d0d0b1f16a59e5ba9886036360cde8b37250c50e
Dennis <dennis.lincango@epn.edu.ec>
2023-01-30 22:10:05 -0500

    Clase Main

---
+++ src/poo/iccr/Evento/Main.java
@@ -0,0 +1,10 @@
+package poo.iccr.Evento;
+
+import poo.Evento.gui.FrmEvento;
+
+public class Main {
+    public static void main(String[] args) {
+        FrmEvento evento = new FrmEvento();
+        evento.setVisible(true);
+    }
+}
